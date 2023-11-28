46071ce923343d47108e2e93b8054f3fd97eb423
Deepanshu Bagotia <61933230+Bagotia16@users.noreply.github.com>
2023-11-29 00:06:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example: Add a simple script to handle a contact form or other interactive elements
+document.addEventListener('DOMContentLoaded', function () {
+    // Your JavaScript code here
+});
